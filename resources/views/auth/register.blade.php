8abb3f23871f00e639cb993377d7629851d9ebc9
Bryan Caicedo <online.jobs.esme@gmail.com>
2025-04-20 19:57:26 -0500

    actualizadon dde idiima del registro de usuarios

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,23 +5,13 @@
 @stop
 
 @section('breadcrumb')
-{{--    <div class="site-hero clearfix">--}}
-        <ol class="breadcrumb float-sm-right ">
-            <li class="breadcrumb-item"> <i class="fas fa-home"> </i> {!! Lang::get('lang.you_are_here') !!} : &nbsp;</li>
-
-            <li><a href="{!! URL::route('post.register') !!}">{!! Lang::get('lang.register') !!}</a></li>
-        </ol>
-{{--    </div>--}}
+    <ol class="breadcrumb float-sm-right ">
+        <li class="breadcrumb-item"> <i class="fas fa-home"> </i> {!! Lang::get('lang.you_are_here') !!} : &nbsp;</li>
+        <li><a href="{!! URL::route('post.register') !!}">{!! Lang::get('lang.registration') !!}</a></li>
+    </ol>
 @stop
-{{--    <div class="site-hero clearfix">--}}
-{{--        <ol class="breadcrumb breadcrumb-custom">--}}
-{{--            <li class="text">{!! Lang::get('lang.you_are_here') !!}: </li>--}}
-{{--            <li><a href="{!! URL::route('/') !!}">{!! Lang::get('lang.home') !!}</a></li>--}}
-{{--        </ol>--}}
-{{--    </div>--}}
 
 @section('content')
-
     @if(Session::has('status'))
     <div class="alert alert-success alert-dismissable">
         <i class="fas fa-check-circle"> </i> <b> {!! Lang::get('lang.success') !!} </b>
@@ -42,15 +32,12 @@
     @endif
 
 <div id="content" class="site-content col-md-12">
-   
     <div id="corewidgetbox" class="wid">
-   
         <div id="wbox" class="widgetrow text-center">
-   
         @if(Auth::user())
         @else
             <span onclick="javascript: window.location.href='{{url('auth/login')}}';">
-                <a href="{{url('auth/login')}}" class="widgetrowitem defaultwidget"  style="background-image:url({{ URL::asset('lb-faveo/media/images/register.png') }})">
+                <a href="{{url('auth/login')}}" class="widgetrowitem defaultwidget" style="background-image:url({{ URL::asset('lb-faveo/media/images/register.png') }})">
                     <span class="widgetitemtitle" style="color: rgb(0, 154, 186)">{!! Lang::get('lang.login') !!}</span>
                 </a>
             </span>
@@ -83,11 +70,8 @@
 
     <div class="d-flex justify-content-center">
         <div class="login-box" style=" width: 490px;">
-            
             <div class="form-border">
-     
                 <div align="center">
-                    
                     <h4 style="background-color: #0084b4;">
                         <a href="http://www.faveohelpdesk.com" class="logo"><img src="{{ asset('lb-faveo/media/images/logo.png')}}" width="100px;" ></a>
                     </h4>    
@@ -95,22 +79,17 @@
                
                 <div>
                     <div class="text-center">
-                        <h3 class="box-title" >{{Lang::get('lang.registration')}}</h3>
-                    </div>   </div>
-
-                <div>
-
-                    <placeholder ="Let’s set up your account in just a couple of steps.">
-                </div>      
+                        <h3 class="box-title">{!! Lang::get('lang.registration') !!}</h3>
+                    </div>
+                </div>
 
                 <!-- form open -->
                 {!!  Form::open(['url'=>'auth/register', 'method'=>'post']) !!}
 
                 <!-- fullname -->
                 <div class="form-group has-feedback {{ $errors->has('full_name') ? 'has-error' : '' }}" style="display: -webkit-box;">
-            
                     {!! Form::text('full_name',null,['placeholder'=>Lang::get('lang.full_name'),'class' => 'form-control']) !!}
-                    <span class="fas fa-user   form-control-feedback" style="top: 9px;left: -25px;color: #6c757d;"></span>
+                    <span class="fas fa-user form-control-feedback" style="top: 9px;left: -25px;color: #6c757d;"></span>
                 </div>
 
                 <!-- Email -->
@@ -138,40 +117,35 @@
                     <div class="col-md-9">
                         <div class="form-group has-feedback {{ $errors->has('mobile') ? 'has-error' : '' }}" style="display: -webkit-box;">
                         {!! Form::text('mobile',null,['placeholder'=>Lang::get('lang.mobile'),'class' => 'form-control']) !!}
-                        <span class="fas fa-phone  form-control-feedback" style="top: 9px;left: -25px;color: #6c757d;"></span>
+                        <span class="fas fa-phone form-control-feedback" style="top: 9px;left: -25px;color: #6c757d;"></span>
                         </div>
                     </div>
                 </div>
                 @else
                     {!! Form::hidden('mobile', null) !!}
                     {!! Form::hidden('code', null) !!}
-
                 @endif
+
                 <!-- Password -->
                 <div class="form-group has-feedback {{ $errors->has('password') ? 'has-error' : '' }}" style="display: -webkit-box;">
-                           
                     {!! Form::password('password',['placeholder'=>Lang::get('lang.password'),'class' => 'form-control']) !!}
-                    <span class="fa fa-lock  form-control-feedback" style="top: 9px;left: -25px;color: #6c757d;"></span>
-
+                    <span class="fa fa-lock form-control-feedback" style="top: 9px;left: -25px;color: #6c757d;"></span>
                 </div>
+
                 <!-- Confirm password -->
                 <div class="form-group has-feedback {{ $errors->has('password_confirmation') ? 'has-error' : '' }}" style="display: -webkit-box;">
-                           
-                    {!! Form::password('password_confirmation',['placeholder'=>Lang::get('lang.retype_password'),'class' => 'form-control']) !!}
+                    {!! Form::password('password_confirmation',['placeholder'=>Lang::get('lang.password_confirmation'),'class' => 'form-control']) !!}
                     <span class="fas fa-sign-in-alt form-control-feedback" style="top: 9px;left: -25px;color: #6c757d;"></span>
-
                 </div>
                 
-                <div >
-                    
+                <div>
                     <button type="submit" class="btn btn-primary btn-block btn-flat" style="width: 100%; hov: #00c0ef; color: #fff">{!! Lang::get('lang.register') !!}</button>
                 </div>
 
                 <div>
-                  
                     <div class="checkbox icheck" align="center">
                         <label class="mb-0">
-                           {{trans('lang.already_got_an_account?')}} <a href="{{url('auth/login')}}" class="text-center">{!! Lang::get('lang.login') !!}</a>
+                            {!! Lang::get('lang.i_already_have_a_membership') !!} <a href="{{url('auth/login')}}" class="text-center">{!! Lang::get('lang.login') !!}</a>
                         </label>
                     </div>
                     <div class="row">
@@ -179,11 +153,10 @@
                             @include('themes.default1.client.layout.social-login')
                         </div>
                     </div>
-                </div><!-- /.col --> 
+                </div>
             </div>
         </div>
     </div>
 </div>
 {!! Form::close()!!}  
-
 @stop
